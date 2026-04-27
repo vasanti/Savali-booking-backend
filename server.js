51b756311a73d15cae54b798e7d2b26236d9e711
Vasanti <vasantiagresar23oct@gmail.com>
2026-04-27 17:47:23 +0530

    razorapi

--- server.js
+++ server.js
@@ -25,7 +25,6 @@ const razorpay = new Razorpay({
   key_id: process.env.RAZORPAY_KEY_ID,
   key_secret: process.env.RAZORPAY_KEY_SECRET
 });
-
 // ==============================
 // ROOT
 // ==============================
